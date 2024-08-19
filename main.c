7c122a5b292297f070784b7fca2b0324a4d15d70
Binchat <binbin.he515@outlook.com>
2024-08-19 15:48:00 +0200

    saving the update change

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
  * @line: pointer to hold the input line
  * @len: variable to hold the buffer size.
  * @numofread:variable to store the number of characters read.
+ * @args: an array to stroe the tokenized arguments
  * Return: Always 0 on success
  */
 int main(void)
@@ -11,19 +12,30 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t numofread;
+	char *args[MAX_ARGS];
 
 	while (1)
 	{
 		printf("Please enter your command >");
-		numofread = getline(&line, &len, stdin);
+		numofread = getline(&line, &len, _stdin);
 		if (numofread == -1)
+		{
+			perror("getline failed")
+					free(line);
+			exit(EXIT_FAILURE);
+		}
+
+		if (numofread > 0 && line[numofread - 1] == '\n')
+		{
+			line[numofread - 1] = '\0';
+		}
+
+		if (_strcmp(line, "exit") == 0)
 		{
 			break;
 		}
 
-		printf("%s", line);
-	}
-	free(line);
+		int num_args = tokenize_command(line, args);
 
-	return (0);
-}
+		return (0);
+	}
