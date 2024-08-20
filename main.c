7d19a1db3ad511c6fdbbd850cf58aae921ae9cd9
Binchat <binbin.he515@outlook.com>
2024-08-20 19:16:08 +0200

    seperate out all modoles can be made into a func

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
  * @len: variable to hold the buffer size.
  * @num_of_read: variable to store the number of characters read.
  * @args: an array to store the tokenized arguments
+ * @MAX_ARGS: the maximum number of command-line arguments.
  * Return: Always 0 on success
  */
 int main(void)
@@ -13,18 +14,21 @@ int main(void)
 	size_t len = 0;
 	ssize_t num_of_read;
 	char *args[MAX_ARGS];
+	list_t *env_head = NULL;
+
+	if (initialize_shell(&buffer, &env_head) != 0)
+	{
+		printf("Failed to initialize shell\n");
+		return (1);
+	}
 
 	while (1)
 	{
+		print_prompt();
+
 		num_of_read = read_command(&line, &len);
 
-		if (num_of_read == -1)
-		{
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-
-		if (_strcmp(line, "exit") == 0)
+		if (handle_input(&line, num_of_read))
 		{
 			break;
 		}
@@ -33,11 +37,9 @@ int main(void)
 
 		if (num_args > 0)
 		{
-			if (execute_command(args) == -1)
-			{
-				perror("Command execution failed!");
-			}
+			execute_command(line, args);
 		}
+
 		else
 		{
 			perror("No valid command entered");
@@ -45,5 +47,6 @@ int main(void)
 	}
 
 	free(line);
+	free_list(env_head);
 	return (0);
 }
