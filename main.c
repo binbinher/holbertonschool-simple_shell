0f7138d57f04d20604c602600404902ca8d23134
binbinher <binbin.he515@outlook.com>
2024-08-21 22:47:19 +0200

    abandoning intialization, handling path

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include "shell.h"
 /**
  * main - Entry Point to Shell
- * @line: pointer to hold the input line
- * @len: variable to hold the buffer size.
- * @num_of_read: variable to store the number of characters read.
- * @args: an array to store the tokenized arguments
- * @MAX_ARGS: the maximum number of command-line arguments.
  * Return: Always 0 on success
  */
 int main(void)
@@ -14,39 +9,37 @@ int main(void)
 	size_t len = 0;
 	ssize_t num_of_read;
 	char *args[MAX_ARGS];
-	list_t *env_head = NULL;
-
-	if (initialize_shell(&buffer, &env_head) != 0)
-	{
-		printf("Failed to initialize shell\n");
-		return (1);
-	}
+	char *command_path;
 
 	while (1)
 	{
 		print_prompt();
-
 		num_of_read = read_command(&line, &len);
 
 		if (handle_input(&line, num_of_read))
 		{
 			break;
 		}
-
 		int num_args = tokenize_command(line, args);
 
 		if (num_args > 0)
 		{
-			execute_command(line, args);
+			command_path = find_command_in_path(args[0]);
+			if (command_path != NULL)
+			{
+				execute_command(line, args);
+				free(command_path);
+			}
+			else
+			{
+				printf("%s: command not found\n", args[0]);
+			}
 		}
-
 		else
 		{
 			perror("No valid command entered");
 		}
 	}
-
 	free(line);
-	free_list(env_head);
 	return (0);
 }
