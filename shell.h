ea0ea890e75230413f828fdfcac0cb3857450ca9
binbinher <binbin.he515@outlook.com>
2024-08-21 23:03:02 +0200

    deleted unused struct type

--- shell.h
+++ shell.h
@@ -13,23 +13,14 @@
 #define BUFF_SIZE 1024
 #define MAX_ARGS 100
 
-/**
- * list_s - a struct type.
- */
-typedef struct list_s
-{
-	char *var_name;
-	char *var_value;
-	struct list_s *next;
-} list_t;
+/* helper functions: */
 
-int initialize_shell(char **line, list_t **env_head);
 int handle_input(char **line, ssize_t num_of_read);
 void print_prompt(void);
 ssize_t read_command(char **line, size_t *len);
 int tokenize_command(char *line, char **args);
+char *find_command_in_path(const char *command);
 int execute_command(char *line, char **args);
-void free_list(list_t *head);
 int _strcmp(const char *str1, const char *str2);
 void free_array(char **array);
 
